26fba66691591261dfdb90e06e51299dfca1b344
TheSixthPhantom <b316009@iiit-bh.ac.in>
2019-08-08 13:37:16 +0530

    adding modules:PAGE_ROUTER

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route,Switch } from 'react-router-dom';
 import './App.css';
-import HomePage from './homepage.component';
-
+import HomePage from './pages/homepage/homepage.component';
+//Prop tunelling: when a prop is only available to the top parent class, and we drill it layers under
+//the children tree, its called prop tunelling and is often a bad practice.
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size, history, match }) => (
+  //allocate different classNames for each, so as to ease styling
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${title}`)}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+//withRouter() method takes in an comonent and returns a similar compoenent which possess the ability
+//to interact with the router dom.
+export default withRouter(MenuItem);
