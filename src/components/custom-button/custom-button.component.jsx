348209721bdbc7975fcf06950d0e6646a46598b0
TheSixthPhantom <b316009@iiit-bh.ac.in>
2019-09-13 14:07:07 +0530

    adding:STYLES_ADD_TO_CART

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './custom-button.styles.scss';
 //buttons can accept the attribute of type=submit like input
 //Props are the attributes that are based while type declaration. props.children contain the characters passed between tags
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${ inverted ? 'inverted' : '' } ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
     {children}
   </button>
 )
